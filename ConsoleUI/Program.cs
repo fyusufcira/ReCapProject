984fab422280a3fee95363760edf63981c58d9ea
Fatih Yusuf Çıra <fatih.cira@boun.edu.tr>
2021-03-07 16:42:04 +0300

    07.03.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,17 +8,19 @@ using Entities.Concrete;
 
 namespace Console
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            Car car1 = new Car() { Description = "opel castra temiz", ModelYear = "2008", DailyPrice = 100, BrandId = 3, ColorId = 1, Id = 1 };
-            carManager.Add(car1);
-            //foreach (var car in carManager.GetAll())
-            //{
-            //    System.Console.WriteLine("Id:"+car.Id+" "+"BrandId: "+car.BrandId + " " +"ColorId: "+car.ColorId + "Price:"+" " +car.DailyPrice + " "+"Description:" +car.Description+"  "+"ModelYear: "+car.ModelYear);
-            //}
+            Car car6 = new Car() { Description = "opel castra temiz", ModelYear = "2008", DailyPrice = 100, BrandId = 3, ColorId = 1, Id = 6 };
+            carManager.Add(car6);
+            Car car5=  new Car() { BrandId = 8, ColorId = 3, DailyPrice = 90, Description = "merhabalar", Id = 5, ModelYear = "2015" };
+            carManager.Add(car5);
+            foreach (var car in carManager.GetAll())
+            {
+                System.Console.WriteLine("Id:"+car.Id+" "+"BrandId: "+car.BrandId + " " +"ColorId: "+car.ColorId + "Price:"+" " +car.DailyPrice + " "+"Description:" +car.Description+"  "+"ModelYear: "+car.ModelYear);
+            }
 
 
 
